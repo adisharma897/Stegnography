a4145bb2349eefc537fad24a436b379face782c4
adisharma897 <adisharma897@gmail.com>
2020-07-29 14:27:09 +0530

    First Commit Upload all files from Eclipse

---
+++ steganography/EmbedMessage/src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Main {
+    public static void main(String arg[]) {
+    NewJFrame a=new NewJFrame();
+    a.setVisible(true);
+    a.setLocationRelativeTo(null);
+    }
+}
